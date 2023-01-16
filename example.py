e861bc7a397a86643589b5dec5eae88e8fe3ca4f
Korzhak <navchalkin@ex.ua>
2023-01-16 19:25:13 +0200

    -

---
+++ example.py
@@ -0,0 +1,42 @@
+from epos4 import Epos4
+from epos4 import definitions as df
+
+e = Epos4('/dev/tty.SLAB_USBtoUART')
+
+
+def move(val):
+    if e.get_fault_state():
+        print("Fault state!")
+        print(repr(e.clear_fault_state()))
+    if not e.get_enable_state():
+        print(f"State is disabled.\nSetting enable state...")
+        print(e.set_enable_state())
+    print(e.move_to_position(val))  # <- Contains print inside method
+
+
+if __name__ == "__main__":
+    # Setting operation mode
+    print("Set op mode")
+    om = e.get_operation_mode().get()
+    if om != df.OM_PPM:
+        print(f"Changing operation mode {om} to {df.OM_PPM}")
+        print(repr(e.set_operation_mode(df.OM_PPM)))
+    print("Operation mode has been set")
+
+    # Setting enable state
+    print("Checking enable state")
+    if not e.get_enable_state():
+        print(f"State is disabled.\nSetting enable state...")
+        print(e.set_enable_state())
+    print(f"Driver has been set to enable state")
+
+    # Moving to position
+    print("Moving to position:")
+    try:
+        for i in range(0xF, 0xFFFF, 0xFF):
+            move(i)
+
+    # Closing connection
+    except KeyboardInterrupt:
+        e.close()
+
---
+++ with_joystick.py
@@ -0,0 +1,59 @@
+from epos4 import Epos4
+from epos4 import definitions as df
+import pygame
+
+pygame.init()
+pygame.joystick.init()
+clock = pygame.time.Clock()
+
+preview_axis = 0
+
+e = Epos4('/dev/tty.SLAB_USBtoUART')
+
+print("Set op mode")
+om = e.get_operation_mode().get()
+if om != df.OM_PPM:
+    print(f"Changing operation mode {om} to {df.OM_PPM}")
+    print(repr(e.set_operation_mode(df.OM_PPM)))
+print("Operation mode has been set")
+
+print("Checking enable state")
+if not e.get_enable_state():
+    print(f"State is disabled.\nSetting enable state...")
+    print(e.set_enable_state())
+
+print(f"Driver has been set to enable state")
+
+def map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN
+        # JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
+            print("Joystick button released.")
+
+    joystick_count = pygame.joystick.get_count()
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+
+        axes = joystick.get_numaxes()
+
+        # for i in range(axes):
+        axis = joystick.get_axis(3)
+        if axis != preview_axis:
+            # print(map(axis, 1, -1, 1019903, 987135))
+            print(e.move_to_position(int(map(axis, -1, 1, 0, 0x8690))))
+            # print(e.move_to_position(int((axis*100 + 250) * 500)))
+            # print(hex(round((axis*100 + 250) * 500)))
+            # print(map(axis, 1, -1, 1019903, 987135))
+        preview_axis = axis
+
+    clock.tick(20)
